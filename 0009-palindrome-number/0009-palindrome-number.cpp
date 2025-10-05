ee2697941aba2904b5dba6b68af961c6473d9519
Humayun Saghir <hr3296726@gmail.com>
2025-10-06 04:35:43 +0500

    Time: 9 ms (8.17%), Space: 10.5 MB (22.59%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        int start = 0;
+        int end = str.length() - 1;
+        while(start <= end){
+            if(str[start] != str[end]){
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+};
