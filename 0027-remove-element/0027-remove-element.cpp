1a22e0a87defc49f42d659124f7eeb6baef53e0e
Humayun Saghir <hr3296726@gmail.com>
2025-10-06 04:38:53 +0500

    Time: 3 ms (1.36%), Space: 11.9 MB (10.11%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                result.push_back(nums[i]);
+            }
+        }
+
+        nums = result;
+
+        return nums.size();
+    }
+};
