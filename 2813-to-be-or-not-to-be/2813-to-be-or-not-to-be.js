54af51bdaa4a901a53d9f5dfe0d5bf14edce5f31
Humayun Saghir <hr3296726@gmail.com>
2025-10-06 04:15:38 +0500

    Time: 41 ms (71.25%), Space: 52.9 MB (88.68%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let obj = {
+        toBe : function(n){
+
+            if(n === val){
+                return true
+            }
+            else{
+                throw new Error("Not Equal")
+            }
+        },
+
+        notToBe : function(n){
+            if(n !== val){
+                return true
+            }
+            else{
+                throw new Error("Equal")
+            }
+        }
+    }
+
+    return obj
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
