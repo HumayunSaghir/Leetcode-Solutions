16d4d1ff3d9d8d10c44b3e88636d53a7ac859acd
Humayun Saghir <hr3296726@gmail.com>
2025-10-07 03:56:46 +0500

    Time: 0 ms (100%), Space: 30.2 MB (99.93%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start) / 2;
+        int ans = -1;
+        while(start <= end){
+            if(nums[mid] == target){
+                ans = mid;
+                break;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+
+            mid = start + (end - start) / 2;
+        }
+
+        return ans;
+    }
+};
