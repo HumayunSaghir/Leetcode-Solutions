4470daf6742e7a012ca8cd7f12f3595b26aa5839
Humayun Saghir <hr3296726@gmail.com>
2025-10-10 02:33:40 +0500

    Time: 22 ms (18.2%), Space: 34 MB (37.64%) - LeetHub

---
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            map[nums[i]]++;
+        }
+
+        int ans = -1;
+
+        for(auto x:map){
+            if(x.second != 1){
+                ans = x.first;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
